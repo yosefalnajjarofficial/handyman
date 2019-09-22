d287cb4775613eb68da7a930879d380979d39c31
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 20:53:33 +0300

    remove spaces

--- server/db/config/build.sql
+++ server/db/config/build.sql
@@ -2,7 +2,7 @@ BEGIN;
 
 DROP TABLE IF EXISTS users, handyman, services, jobs;
 
-CREATE TABLE users (
+CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
