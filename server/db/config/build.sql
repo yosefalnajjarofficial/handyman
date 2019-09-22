7b5aea67da5e3f4121a9284b8a4fa4752b7fe811
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 19:31:46 +0300

    remove spaces and change some fields types relates #3

--- server/db/config/build.sql
+++ server/db/config/build.sql
@@ -1,8 +1,8 @@
 BEGIN;
 
-DROP TABLE IF EXISTS users,handyman,services CASCADE;
+DROP TABLE IF EXISTS users,handyman,services jobs;
 
-  CREATE TABLE users (
+CREATE TABLE users (
   id SERIAL PRIMARY KEY NOT NULL,
   username VARCHAR(255) NOT NULL,
   email VARCHAR(255) UNIQUE NOT NULL,
@@ -17,27 +17,26 @@ CREATE TABLE services(
   id SERIAL PRIMARY KEY NOT NULL,
   name VARCHAR(255) NOT NULL
 );
+
 CREATE TABLE handyman(
   handyman_id INTEGER PRIMARY KEY REFERENCES users(id),
   job_title INTEGER REFERENCES services(id),
   hour_rate INTEGER NOT NULL,
-  description VARCHAR(255) NOT NULL
+  description VARCHAR NOT NULL
 );
 
-
-
 CREATE TABLE jobs (
-id SERIAL PRIMARY KEY NOT NULL, 
-client_id INTEGER REFERENCES users(id), 
-handyman_id INTEGER REFERENCES handyman(handyman_id), 
-description VARCHAR(255) NOT NULL, 
-dead_line DATE NOT NULL, 
-price INTEGER NOT NULL, 
-status VARCHAR(255) NOT NULL, 
-message VARCHAR(255), 
-street VARCHAR(255) NOT NULL, 
-building_number VARCHAR(255) NOT NULL, 
-flat_number VARCHAR(255) NOT NULL
+  id SERIAL PRIMARY KEY NOT NULL, 
+  client_id INTEGER REFERENCES users(id), 
+  handyman_id INTEGER REFERENCES handyman(handyman_id), 
+  description VARCHAR NOT NULL, 
+  dead_line DATE NOT NULL, 
+  price INTEGER NOT NULL, 
+  status VARCHAR(255) NOT NULL, 
+  message VARCHAR, 
+  street VARCHAR(255) NOT NULL, 
+  building_number VARCHAR(255) NOT NULL, 
+  flat_number VARCHAR(255) NOT NULL
 );
 
 COMMIT;
