05384e633bf63dd0dd7df31579b220f50c6c37fc
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 21:10:22 +0300

    remove blank line

--- server/db/config/fakeData.sql
+++ server/db/config/fakeData.sql
@@ -10,7 +10,6 @@ INSERT INTO services (name) VALUES ('cooker');
 INSERT INTO handyman (handyman_id, job_title,hour_rate,description) 
 VALUES (1,1,15,'good worker');
 
-
 INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status,message,street,building_number,flat_number) 
 VALUES (2,1,'cook the food','12-12-2020',15,'pending','build my house','omar el mukhtar','123','12');
 
