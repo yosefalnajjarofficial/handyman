207fef8ec1d04fdcc3c7883b50264b96126b815d
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 21:36:52 +0300

    add more fake data

--- server/db/config/fakeData.sql
+++ server/db/config/fakeData.sql
@@ -13,4 +13,10 @@ VALUES (1,1,15,'good builder');
 INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status,message,street,building_number,flat_number) 
 VALUES (2,1,'build the house','12-12-2020',15,'pending','build my house','omar el mukhtar','123','12');
 
+INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status,message,street,building_number,flat_number) 
+VALUES (2,1,'build the flat','20-1-2020',15,'accepted','build the flat','omar el mukhtar','12','142');
+
+INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status,message,street,building_number,flat_number) 
+VALUES (2,1,'build the flat','10-12-2019',20,'decliend','build the room','omar el mukhtar','132','1422');
+
 COMMIT;
