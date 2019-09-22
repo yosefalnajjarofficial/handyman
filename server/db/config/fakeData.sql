406af9b2d0636f81fcff55f6b08bb520a361a3c9
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 21:29:47 +0300

    remove to cocker

--- server/db/config/fakeData.sql
+++ server/db/config/fakeData.sql
@@ -11,6 +11,6 @@ INSERT INTO handyman (handyman_id, job_title,hour_rate,description)
 VALUES (1,1,15,'good builder');
 
 INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status,message,street,building_number,flat_number) 
-VALUES (2,1,'cook the food','12-12-2020',15,'pending','build my house','omar el mukhtar','123','12');
+VALUES (2,1,'build the house','12-12-2020',15,'pending','build my house','omar el mukhtar','123','12');
 
 COMMIT;
