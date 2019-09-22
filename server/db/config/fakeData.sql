7f5f7faf9142bb7c252840b645441f35ae3ca945
mossa-Sammer <mossa708496@gmail.com>
2019-09-22 18:53:48 +0300

    make the database connection and insert fake data relates #46

---
+++ server/db/config/fakeData.sql
@@ -0,0 +1,11 @@
+BEGIN;
+INSERT INTO users (username,email,phone,password,country,city,is_handyman) VALUES ('mossa','mossa123@gmail.com',11111111,'mmm123','palestine','gaza',true);
+
+INSERT INTO handyman (handyman_id, job_title,hour_rate,description) VALUES (1,'cooker',15,'good worker');
+
+INSERT INTO services (name) VALUES ('cooker');
+
+INSERT INTO jobs (client_id,handyman_id,description,dead_line,price,status) VALUES (2,1,'cook the food','12-12-2020',15,'pending')
+
+
+COMMIT;
