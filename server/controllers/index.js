d12e4a96936b8eed0cd179557a4086271269b16c
hashemt <hashemta77@gmail.com>
2019-09-23 22:54:45 +0300

    require unlockCookie middleware in this file Relates #40

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,7 +3,9 @@ const express = require('express');
 const router = express.Router();
 
 const { getUserJobs } = require('./routes');
+const { unlockCookie } = require('./middleware/unlockCookie');
 
+router.use(unlockCookie);
 router.get('/jobs', getUserJobs);
 
 module.exports = router;
