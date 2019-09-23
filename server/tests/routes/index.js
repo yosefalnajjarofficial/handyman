f93500efca4d4c91e1172697be87f66c574344ff
FadiAlamassi <alamassifadi@gmail.com>
2019-09-23 20:27:00 +0300

    create tests for the hire route and require it in the index file relates #42

--- server/tests/routes/index.js
+++ server/tests/routes/index.js
@@ -1,7 +1,1 @@
 require('./postJob');
-const tape = require('tape');
-
-tape('Simple pass test', (t) => {
-  t.equal(1, 1, 'one should equal one');
-  t.end();
-});
--- server/tests/routes/postJob.js
+++ server/tests/routes/postJob.js
@@ -1,27 +1,34 @@
 const tape = require('tape');
-const supertest = require('supertest');
+const request = require('supertest');
 const app = require('../../app');
 
 tape('Test Hire route', (t) => {
-  supertest(app)
-    .post('/hire')
-    .send({
-      handymanId: 2222,
-      deadline: new Date(),
-      price: 6,
-      description: 'build a house',
-      street: 'Tal Elhawa',
-      buildingNumber: '111',
-      flatNumber: '222',
-    })
-    .set('Cookie', ['clientId=9999'])
+  const jobContract = {
+    handymanId: 1,
+    deadline: new Date(),
+    price: 6,
+    description: 'build a house',
+    street: 'Tal Elhawa',
+    status: 'pending',
+    buildingNumber: '111',
+    flatNumber: '222',
+  };
+  request(app)
+    .post('/api/v1/hire')
+    .send(jobContract)
+    .set('Accept', 'application/json')
+    .set('Cookie', ['clientId=2'])
     .expect('Content-Type', /json/)
     .expect(200)
     .end((err, res) => {
-      if (err) t.error(err);
-      else {
+      if (err) {
+        t.error(err);
+        t.end();
+      } else {
         t.ok(res);
         t.end();
       }
     });
 });
+
+tape.onFinish(() => process.exit(0));
