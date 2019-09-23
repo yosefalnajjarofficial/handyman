09d2cc2de498ababd58f88819948358751331e9b
FadiAlamassi <alamassifadi@gmail.com>
2019-09-23 20:26:26 +0300

    create tests for the database and require it in the index file relates #42

--- server/tests/queries/postJob.js
+++ server/tests/queries/postJob.js
@@ -1,3 +1,31 @@
 const tape = require('tape');
 const dbBuild = require('../../db/config/build');
-const { postHire } = require('../../db/queries/postJob');
+const insertFakeData = require('../../db/config/insertFakeData');
+const { postJob } = require('../../db/queries/postJob');
+
+tape('test adding the job contract', async (t) => {
+  const fakeData = {
+    clientId: 2,
+    handymanId: 1,
+    deadline: new Date(),
+    price: 6,
+    description: 'build a house',
+    street: 'Tal Elhawa',
+    status: 'pending',
+    buildingNumber: '111',
+    flatNumber: '222',
+  };
+  const expected = 2;
+  try {
+    await dbBuild();
+    await insertFakeData();
+    const result = await postJob(fakeData);
+    t.equals(result.rows[0].client_id, expected, 'client id should be 2');
+    t.end();
+  } catch (err) {
+    t.error(err);
+    t.end();
+  }
+});
+
+tape.onFinish(() => process.exit(0));
