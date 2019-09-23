3816bbf814bfe2e5dcf645a845f6f245965c4cea
hashemt <hashemta77@gmail.com>
2019-09-23 11:33:18 +0300

    add router.get to /jobs with created function getUserJobs relates #40

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const { getUserJobs } = require('./routes');
+
 const router = express.Router();
+
+router.get('/jobs', getUserJobs);
 
 module.exports = router;
