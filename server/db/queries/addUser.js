92e150ca9902ea7b0fe59e7edfffbdad5ada20c0
mossa-Sammer <mossa708496@gmail.com>
2019-09-23 12:07:22 +0300

    complete the database query test

--- server/db/queries/addUser.js
+++ server/db/queries/addUser.js
@@ -5,16 +5,18 @@ const addUser = ({
   email,
   phone,
   password,
+  country,
   city,
   isHandyman,
 }) => {
   const sql = {
-    text: 'INSERT INTO users (username,email,phone,password,country,city,is_handyman) VALUES ($1,$2,$3,$4,$5,$6,$7) returning id;',
+    text: 'INSERT INTO users (username,email,phone,password,country,city,is_handyman) VALUES ($1,$2,$3,$4,$5,$6,$7) returning *;',
     values: [
       username,
       email,
       phone,
       password,
+      country,
       city,
       isHandyman,
     ],
--- server/tests/queries/index.js
+++ server/tests/queries/index.js
@@ -10,7 +10,29 @@ tape('Simple pass test', (t) => {
 tape('add a user to table users', (t) => {
   build()
     .then((res) => {
-      addUser('');
+      addUser({
+        username: 'mossa1',
+        email: 'mossa700@gmail.com',
+        phone: 123123123,
+        password: 'sss',
+        country: 'palestine',
+        city: 'gaza',
+        isHandyman: false,
+      }).then((res) => {
+        console.log(res.rows[0]);
+        t.deepEqual(res.rows[0], {
+          id: 1,
+          username: 'mossa1',
+          email: 'mossa700@gmail.com',
+          phone: 123123123,
+          password: 'sss',
+          country: 'palestine',
+          city: 'gaza',
+          is_handyman: false,
+        }, 'should pass');
+        t.end();
+      });
     })
     .catch((err) => console.log(err));
 });
+tape.onFinish(() => { process.exit(0); });
