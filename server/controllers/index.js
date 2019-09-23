952c53a643fc224bd1fee942752ed60919f01a51
FadiAlamassi <alamassifadi@gmail.com>
2019-09-23 22:22:00 +0300

    add new line between the core module and the local relates #35

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+
 const logout = require('../controllers/routes/getLogout');
 
 const router = express.Router();
+
 router.get('/logout', logout);
 
 module.exports = router;
