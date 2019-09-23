a0a918902130f3170e19e5ab32a3ede9b7226a00
hashemt <hashemta77@gmail.com>
2019-09-23 13:02:29 +0300

    arrangement the file relates #40

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 
-const { getUserJobs } = require('./routes');
-
 const router = express.Router();
+const { getUserJobs } = require('./routes');
 
 router.get('/jobs', getUserJobs);
 
