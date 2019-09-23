7cd05e784311345cfa4a0783952290f8b77dba22
hashemt <hashemta77@gmail.com>
2019-09-23 13:19:44 +0300

    replace the promise with async await Relates #40

--- server/tests/queries/getJobQuery.js
+++ server/tests/queries/getJobQuery.js
@@ -4,11 +4,11 @@ const dbBuild = require('../../db/config/build.js');
 const insertFakeData = require('../../db/config/insertFakeData');
 const { getJobs } = require('../../db/queries/getJobs');
 
-exports.getJobsQuery = test('testing for the length of jobs information from database', (t) => {
+exports.getJobsQuery = test('testing for the length of jobs information from database', async (t) => {
   const expected = 11;
-  dbBuild()
-    .then(() => insertFakeData())
-    .then(() => getJobs(2))
-    .then((jobs) => t.deepEqual(Object.keys(jobs.rows[0]).length, expected, 'should be the same value which shows the number of keys'))
-    .then(() => t.end());
+  await dbBuild();
+  await insertFakeData();
+  const jobs = await getJobs(2);
+  t.deepEqual(Object.keys(jobs.rows[0]).length, expected, 'should be the same value which shows the number of keys');
+  t.end();
 });
