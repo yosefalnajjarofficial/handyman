d289276a3857c8813ca35b806d45a68e15d929d9
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 13:19:10 +0300

    use the client id returned from unLockCookie relates #42

--- server/controllers/routes/postJob.js
+++ server/controllers/routes/postJob.js
@@ -2,12 +2,10 @@ const { postJob } = require('../../db/queries/postJob');
 const { newJobSchema } = require('../ValidationSchemas/newJobSchema');
 
 exports.postJob = async (req, res, next) => {
-  const { clientId } = req.cookies;
-  req.body.clientId = clientId;
+  req.body.clientId = req.user.id;
   try {
     await newJobSchema.validate(req.body);
     await postJob(req.body);
-    res.set('Content-Type', 'application/json');
     res.send({ message: 'success', statusCode: 200 });
   } catch (err) {
     if (err.name === 'ValidationError') {
