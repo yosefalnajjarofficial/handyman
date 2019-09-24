c2f46540c35ed3aad1cc1e094ba6d83e26a712a0
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 13:15:08 +0300

    require unlockCoolie middleware relates #42

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 
 const { postJob } = require('./routes/postJob');
 const logout = require('../controllers/routes/getLogout');
+const { unlockCookie } = require('./middleware/unlockCookie');
 
 const router = express.Router();
 
+router.use(unlockCookie);
 router.post('/hire', postJob);
 router.get('/logout', logout);
 
