01550f2d19702ff0f6cf01369804559e794bfe6d
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 14:44:30 +0300

    test the keys of the response insted t.ok relates #42

--- server/tests/routes/postJob.js
+++ server/tests/routes/postJob.js
@@ -3,7 +3,7 @@ const request = require('supertest');
 
 const app = require('../../app');
 
-tape('Test Hire route', (t) => {
+tape('Test Hire route', async (t) => {
   const jobContract = {
     handymanId: 1,
     deadline: new Date(),
@@ -17,7 +17,6 @@ tape('Test Hire route', (t) => {
   request(app)
     .post('/api/v1/hire')
     .send(jobContract)
-    .set('Accept', 'application/json')
     .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJmYWRpMTIzQGdtYWlsIiwidXNlcm5hbWUiOiJmYWRpIiwiaXNIYW5keW1hbiI6ZmFsc2UsImlhdCI6MTU2OTI2Nzg0OH0.wnVIwaHXQ6jpPmsx4FEnGgLRWanBINHvPpiaqBLA1YE'])
     .expect('Content-Type', /json/)
     .expect(200)
@@ -26,7 +25,7 @@ tape('Test Hire route', (t) => {
         t.error(err);
         t.end();
       } else {
-        t.ok(res);
+        t.equals(Object.keys(res.body.data).length, 11, 'Keys number shuld be 11');
         t.end();
       }
     });
