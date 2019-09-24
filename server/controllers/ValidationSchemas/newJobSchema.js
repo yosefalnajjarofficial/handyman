23d9f3efdf36dadd9ed9666d3d1d26fb8b589ab3
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 14:41:43 +0300

    remove minimum value of description and move required to the end of line in price relates #42

--- server/controllers/ValidationSchemas/newJobSchema.js
+++ server/controllers/ValidationSchemas/newJobSchema.js
@@ -2,8 +2,8 @@ const yup = require('yup');
 
 exports.newJobSchema = yup.object({
   deadline: yup.date().min(new Date()),
-  price: yup.number().positive().required().min(5),
-  description: yup.string().min(5).required(),
+  price: yup.number().positive().min(5).required(),
+  description: yup.string().required(),
   street: yup.string().required(),
   buildingNumber: yup.string().required(),
   flatNumber: yup.string().required(),
