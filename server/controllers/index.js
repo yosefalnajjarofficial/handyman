8c911ccf61a74d6a843603e98c31a7bf1d1a2ef3
fares98 <faresalhello21@gmail.com>
2019-09-24 15:09:54 +0300

    add route for function getServices with method git Relates #37

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -3,8 +3,11 @@ const express = require('express');
 const router = express.Router();
 
 const { unlockCookie } = require('./middleware/unlockCookie');
-const { profile, logout, userJobs } = require('./routes');
+const {
+  profile, logout, userJobs, getServices,
+} = require('./routes');
 
+router.get('/services', getServices);
 router.use(unlockCookie);
 router.get('/profile/:id', profile);
 router.get('/jobs', userJobs);
