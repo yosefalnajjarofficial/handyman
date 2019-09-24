72226dfad49ee271bc50fc44b5f1bd08f605734a
fares98 <faresalhello21@gmail.com>
2019-09-24 15:58:54 +0300

    add next to the function and remove console Relates #37

--- server/controllers/routes/getServices.js
+++ server/controllers/routes/getServices.js
@@ -1,10 +1,10 @@
 const { getServices } = require('../../db/queries/getServices');
 
-module.exports = async (req, res) => {
+module.exports = async (req, res, next) => {
   try {
     const services = await getServices();
     res.send({ data: services.rows });
   } catch (err) {
-    console.log(err);
+    next(err);
   }
 };
