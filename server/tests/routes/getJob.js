9d917b1340d02bdc70fa5439947fd883829bd9f4
fares98 <faresalhello21@gmail.com>
2019-09-24 16:06:28 +0300

    change file destructure Relates #37

--- server/tests/routes/getJob.js
+++ server/tests/routes/getJob.js
@@ -5,7 +5,7 @@ const app = require('../../app');
 const dbBuild = require('../../db/config/build.js');
 const insertFakeData = require('../../db/config/insertFakeData');
 
-exports.getJob = test('testing for route /jobs at GET method', async (t) => {
+test('testing for route /jobs at GET method', async (t) => {
   await dbBuild();
   await insertFakeData();
   supertest(app)
