635eddfac4e3298967ac903fb08374643046afb7
hashemt <hashemta77@gmail.com>
2019-09-24 14:12:10 +0300

    edit test

--- server/tests/routes/getProfile.js
+++ server/tests/routes/getProfile.js
@@ -5,11 +5,12 @@ const app = require('../../app');
 const dbBuild = require('../../db/config/build.js');
 const insertFakeData = require('../../db/config/insertFakeData');
 
-exports.getProfile = test('testing for route /jobs at GET method', async (t) => {
+test('testing for route /profile at GET method', async (t) => {
   await dbBuild();
   await insertFakeData();
   supertest(app)
     .get('/api/v1/profile/1')
+    .set('Cookie', ['jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MiwiZW1haWwiOiJmYWRpMTIzQGdtYWlsIiwidXNlcm5hbWUiOiJmYWRpIiwiaXNIYW5keW1hbiI6ZmFsc2UsImlhdCI6MTU2OTI2Nzg0OH0.wnVIwaHXQ6jpPmsx4FEnGgLRWanBINHvPpiaqBLA1YE'])
     .expect(200)
     .end((err, res) => {
       if (err) {
