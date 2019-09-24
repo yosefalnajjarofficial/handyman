9f04a5957ad7f1a13709530634cb88a4ecd229b0
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 10:56:15 +0300

    format code relates #42

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
-const { postJob } = require('./routes/postJob');
 
+const { postJob } = require('./routes/postJob');
 const logout = require('../controllers/routes/getLogout');
 
 const router = express.Router();
-router.post('/hire', postJob);
 
+router.post('/hire', postJob);
 router.get('/logout', logout);
 
 module.exports = router;
