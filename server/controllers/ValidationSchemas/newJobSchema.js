149d80516895f5381350a4ecaf6a6da70813e032
FadiAlamassi <alamassifadi@gmail.com>
2019-09-24 13:18:00 +0300

    format code relates #42

--- server/controllers/ValidationSchemas/newJobSchema.js
+++ server/controllers/ValidationSchemas/newJobSchema.js
@@ -2,15 +2,8 @@ const yup = require('yup');
 
 exports.newJobSchema = yup.object({
   deadline: yup.date().min(new Date()),
-  price: yup
-    .number()
-    .positive()
-    .required()
-    .min(5),
-  description: yup
-    .string()
-    .min(5)
-    .required(),
+  price: yup.number().positive().required().min(5),
+  description: yup.string().min(5).required(),
   street: yup.string().required(),
   buildingNumber: yup.string().required(),
   flatNumber: yup.string().required(),
