6941dc080725840d16f6b027edf7392bccc95124
fares98 <faresalhello21@gmail.com>
2019-09-25 10:21:01 +0300

    remove export file Relates #37

--- server/controllers/routes/index.js
+++ server/controllers/routes/index.js
@@ -2,5 +2,4 @@ exports.profile = require('./getProfile');
 exports.logout = require('./getLogout');
 exports.userJobs = require('./getJobs');
 exports.services = require('./getServices');
-exports.profile = require('./getProfile');
 exports.oneService = require('./getOneService');
