99febe2789eee299d7079f17a3366dbb9137e2e8
hashemt <hashemta77@gmail.com>
2019-09-25 20:07:07 +0300

    create Job Card and make it style Relates #31

--- client/src/components/common/JobCard/index.js
+++ client/src/components/common/JobCard/index.js
@@ -3,26 +3,48 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const JobCard = props => {
-  const {
-    description,
-    dead_line,
-    price,
-    status,
-    message,
-    street,
-    building_number,
-    flat_number,
-  } = props;
+const JobCard = ({
+  username,
+  description,
+  deadLine,
+  price,
+  status,
+  message,
+  street,
+  buildingNumber,
+  flatNumber,
+}) => {
   return (
-    <React.Fragment>
-      <h3></h3>
-      <h4>Description: </h4>
-      <h4>Time:</h4>
-      <h4>price:</h4>
-      <h4>Adress:</h4>
-      <h4>Message:</h4>
-      <h4>Status:</h4>
-    </React.Fragment>
+    <div className="card">
+      <h3 className="card__username">{username}</h3>
+      <h4 className="card__body">Description: {description} </h4>
+      <h4 className="card__body">Time: {deadLine}</h4>
+      <h4 className="card__body">price: {price}$</h4>
+      <h4 className="card__body">
+        Adress:{street} / {buildingNumber} / {flatNumber}
+      </h4>
+      {message ? <h4 className="card__body">Message :{message}</h4> : ''}
+      <h4 className="card__status">
+        <h4>Status: </h4>
+        <h4 className={status}> {status}</h4>
+      </h4>
+    </div>
   );
 };
+
+JobCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  deadLine: PropTypes.string.isRequired,
+  price: PropTypes.number.isRequired,
+  status: PropTypes.string.isRequired,
+  message: PropTypes.string,
+  street: PropTypes.string.isRequired,
+  buildingNumber: PropTypes.number.isRequired,
+  flatNumber: PropTypes.string.isRequired,
+};
+JobCard.defaultProps = {
+  message: null,
+};
+
+export default JobCard;
