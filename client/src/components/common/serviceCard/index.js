387b0f6788bbf1b66be06f3d4ddd3fef1e239b52
FadiAlamassi <alamassifadi@gmail.com>
2019-09-25 16:09:52 +0300

    make the alternative text of image dynamic relates #27

--- client/src/components/common/serviceCard/index.js
+++ client/src/components/common/serviceCard/index.js
@@ -4,25 +4,25 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const ServiceCard = ({ serviceTitle, onServiceClick }) => {
+const ServiceCard = ({ serviceName, onServiceClick }) => {
   return (
     <div role="button" className="card service-card" onClick={onServiceClick}>
       <div className="image-container">
         <img
           className="card-image"
           src="https://image.flaticon.com/icons/svg/307/307892.svg"
-          alt="Service Card"
+          alt={`${serviceName} card`}
         />
       </div>
       <div className="tilte-container">
-        <h1 className="service-title">{serviceTitle}</h1>
+        <h1 className="service-title">{serviceName}</h1>
       </div>
     </div>
   );
 };
 
 ServiceCard.propTypes = {
-  serviceTitle: PropTypes.string.isRequired,
+  serviceName: PropTypes.string.isRequired,
   onServiceClick: PropTypes.func.isRequired,
 };
 
