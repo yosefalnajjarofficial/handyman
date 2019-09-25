08b2a8a6dd83f0bcbac545564f50bb2fd1b6a70e
hashemt <hashemta77@gmail.com>
2019-09-25 22:45:58 +0300

    create profile card component Relates #30

---
+++ client/src/components/common/ProfileCard/index.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './style.css';
+import handymanImg from '../../../assets/handyman.png';
+import Button from '../Button';
+
+const ProfileCard = props => {
+  const { username, service, country, city, hourRate, bio } = props;
+  return (
+    <div className="card">
+      <div className="card__header">
+        <img className="card__header--img" src={handymanImg} alt="handyman" />
+        <div className="card__header--info">
+          <h3 className>{username}</h3>
+          <h4>{service}</h4>
+          <h4>
+            {country}, {city}
+          </h4>
+          <h4>{hourRate}$/hr</h4>
+          <div className="btn-container">
+            <Button
+              className="message"
+              children="massege"
+              buttonFunction={undefined}
+            />
+            <Button
+              className="hire"
+              children="hire"
+              buttonFunction={undefined}
+            />
+          </div>
+        </div>
+      </div>
+      <h4 className="bio">{bio}</h4>
+    </div>
+  );
+};
+
+ProfileCard.propTypes = {
+  username: PropTypes.string.isRequired,
+  service: PropTypes.string.isRequired,
+  country: PropTypes.string.isRequired,
+  city: PropTypes.string.isRequired,
+  hourRate: PropTypes.number.isRequired,
+  bio: PropTypes.string.isRequired,
+};
+
+export default ProfileCard;
