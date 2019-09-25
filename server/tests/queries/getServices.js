e82600dfc481f491b17fe456ce0b54105cfdbe86
fares98 <faresalhello21@gmail.com>
2019-09-25 10:34:24 +0300

    rename the object from obj to services Relates #37

--- server/tests/queries/getServices.js
+++ server/tests/queries/getServices.js
@@ -3,8 +3,8 @@ const test = require('tape');
 const { getServices } = require('../../db/queries/getServices');
 
 test('testing for getServices query database', async (t) => {
-  const obj = await getServices();
-  const actual = Object.keys(obj.rows[0]).length;
+  const services = await getServices();
+  const actual = Object.keys(services.rows[0]).length;
   t.equal(actual, 2, 'Number of Keys shoud be 2');
   t.end();
 });
