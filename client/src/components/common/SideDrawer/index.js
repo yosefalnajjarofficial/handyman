39840635fe940354b37312dde3c7b74e38c1cc4b
fares98 <faresalhello21@gmail.com>
2019-09-26 09:50:15 +0300

    add the validation for the component Relates #34

--- client/src/components/common/SideDrawer/index.js
+++ client/src/components/common/SideDrawer/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './style.css';
 
-const SideDrawer = props => {
+const SideDrawer = ({ loggedIN }) => {
   const notLogedArr = ['Home', 'Services', 'Sign Up', 'Log In'];
   const logedArr = [
     <img
-      src='https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg'
+      src="https://png.pngtree.com/element_our/png/20181206/users-vector-icon-png_260862.jpg"
       alt="UserImage"
       className="img"
     />,
@@ -26,9 +27,11 @@ const SideDrawer = props => {
 
   return (
     <aside className="aside">
-      <ul className="ul">{props ? items(logedArr) : items(notLogedArr)}</ul>
+      <ul className="ul">{loggedIN ? items(logedArr) : items(notLogedArr)}</ul>
     </aside>
   );
 };
+
+SideDrawer.propTypes = { loggedIN: PropTypes.bool.isRequired };
 
 export default SideDrawer;
