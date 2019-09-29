9f36db08b7d6e9dd9998927141c916a076a544a7
FadiAlamassi <alamassifadi@gmail.com>
2019-09-29 13:42:36 +0300

    remove htmlFor from the props and make it dynamic move destructuring inside the map relates #90

--- client/src/components/common/selectOption/index.js
+++ client/src/components/common/selectOption/index.js
@@ -3,18 +3,20 @@ import PropTypes from 'prop-types';
 
 import './style.css';
 
-const selectOption = ({ htmlFor, label, selectName, options }) => {
-  const { id, name } = options;
+const selectOption = ({ label, selectName, options }) => {
   return (
     <div className="select__container">
-      <label htmlFor={htmlFor} className="select__label">
+      <label htmlFor={selectName} className="select__label">
         {label}
-        <select className="select__tag" id={htmlFor} name={selectName}>
-          {options.map(option => (
-            <option className="select__option" key={id} value={id}>
-              {name}
-            </option>
-          ))}
+        <select className="select__tag" id={selectName} name={selectName}>
+          {options.map(option => {
+            const { id, name } = option;
+            return (
+              <option className="select__option" key={id} value={id}>
+                {name}
+              </option>
+            );
+          })}
         </select>
       </label>
     </div>
@@ -22,7 +24,6 @@ const selectOption = ({ htmlFor, label, selectName, options }) => {
 };
 
 selectOption.propTypes = {
-  htmlFor: PropTypes.string.isRequired,
   label: PropTypes.string.isRequired,
   selectName: PropTypes.string.isRequired,
   options: PropTypes.arrayOf(PropTypes.string).isRequired,
