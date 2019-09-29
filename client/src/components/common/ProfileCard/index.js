f11eba9f150e281264d369b178dd3e7f9fc82b88
hashemt <hashemta77@gmail.com>
2019-09-29 22:23:44 +0300

    edit style of the stars Relates #14

--- client/src/components/common/ProfileCard/index.js
+++ client/src/components/common/ProfileCard/index.js
@@ -36,9 +36,11 @@ const ProfileCard = ({
         </div>
       </div>
       <div className="card-btnContainer">
-        {starNumber.map(() => (
-          <Star />
-        ))}
+        <div className="stars">
+          {starNumber.map(() => (
+            <Star />
+          ))}
+        </div>
         <Button className="card-btn message" onClick={onClickMessage}>
           Massege
         </Button>
