742e3d891aab5f6a37ebd8821df5c7bec886f331
hashemt <hashemta77@gmail.com>
2019-09-30 13:42:12 +0300

    sending not found when the handyman not found Relates #14

--- server/controllers/routes/getProfile.js
+++ server/controllers/routes/getProfile.js
@@ -3,7 +3,8 @@ const { getProfile } = require('../../db/queries/getProfile');
 module.exports = async (req, res, next) => {
   try {
     const profileData = await getProfile(req.params.id);
-    res.send({ data: profileData.rows[0], statusCode: 200 });
+    if (profileData.rows[0]) res.send({ data: profileData.rows[0], statusCode: 200 });
+    else res.send({ message: 'user not found', statusCode: 404 });
   } catch (error) {
     next(error);
   }
