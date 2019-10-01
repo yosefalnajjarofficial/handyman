e4e55964548316a20b8f54dbb3ce7bdc015de570
hashemt <hashemta77@gmail.com>
2019-10-01 11:07:18 +0300

    edit getOneService query to get handyman id and edit it test Relates #39

--- server/db/queries/getOneService.js
+++ server/db/queries/getOneService.js
@@ -1,3 +1,3 @@
 const connection = require('../config/connection');
 
-exports.getOneService = (id) => connection.query('select t1.name,t2.description,t2.hour_rate,t3.username,t3.phone from services t1 inner join handyman t2 ON t2.job_title = t1.id inner join users t3 on t3.id = t2.handyman_id where t2.job_title=$1', [id]);
+exports.getOneService = (id) => connection.query('select t1.name,t2.description,t2.hour_rate, t2.handyman_id, t3.username,t3.phone from services t1 inner join handyman t2 ON t2.job_title = t1.id inner join users t3 on t3.id = t2.handyman_id where t2.job_title=$1', [id]);
