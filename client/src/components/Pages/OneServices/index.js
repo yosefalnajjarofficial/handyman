aa750e145af18dfc6c6dc9d7de1ed6b3f058a84a
fares98 <faresalhello21@gmail.com>
2019-10-01 10:06:21 +0300

    create OneServices-page Relates #13

--- client/src/components/Pages/OneServices/index.js
+++ client/src/components/Pages/OneServices/index.js
@@ -11,45 +11,53 @@ import './style.css';
 class Services extends Component {
   state = {
     oneServicesData: [],
-    redirect: false,
+    id: 1,
+    name: 'building',
   };
 
   async componentDidMount() {
+    const { id } = this.state;
     try {
-      const services = await axios.get(`api/v1/services`);
-      const result = services.data.data;
-      this.setState({ oneServicesData: result });
+      const services = await axios.get(`api/v1/service/${id}`);
+      this.setState({ oneServicesData: services.data.data });
     } catch (err) {
       console.log(err);
     }
   }
 
-  setRedirect = () => {
-    this.setState({ redirect: true });
-  };
-
-  renderRedirect = () => {
-    const { redirect } = this.state;
-    if (redirect) {
-      return <Redirect to="/" />;
-    }
-    return null;
-  };
-
   render() {
-    const { oneServicesData } = this.state;
+    const { oneServicesData, name } = this.state;
     return (
       <section>
-        {this.renderRedirect()}
         <SearchInput name="search" placeholder="Search" />
-        <ul className="card-parent">
-          {oneServicesData.map(element => (
-            <span>
-              <HandymanCard />
-              <SliderCard />
-            </span>
-          ))}
-        </ul>
+
+        <div className="top-rated-cards">
+          {oneServicesData.map(element =>
+            element.name === name ? (
+              <SliderCard
+                key={element}
+                handymanName={element.username}
+                handymanBio={element.description}
+                handymanService={element.name}
+                rate={3}
+              />
+            ) : null
+          )}
+        </div>
+
+        <div className="handyman-cards">
+          {oneServicesData.map(element =>
+            element.name === name ? (
+              <HandymanCard
+                key={element}
+                HandymanName={element.username}
+                handymanBio={element.description}
+                handymanService={element.name}
+                rate={3}
+              />
+            ) : null
+          )}
+        </div>
       </section>
     );
   }
