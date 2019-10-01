2856c7007125efd8d7dec4cafbdeed78cf96e939
hashemt <hashemta77@gmail.com>
2019-10-01 10:54:13 +0300

    edit the spaces

--- client/src/components/Pages/NotFound/index.js
+++ client/src/components/Pages/NotFound/index.js
@@ -4,14 +4,14 @@ import { Link } from 'react-router-dom';
 import pageNotFound from '../../../assets/404.png';
 import './style.css';
 
-const NotFound = props => {
+const NotFound = () => {
   return (
-    <div>
+    <section>
       <img src={pageNotFound} alt="Not Found 404" className="notFound__img" />
       <Link to="/" className="error404__goHome">
         Return to Home Page
       </Link>
-    </div>
+    </section>
   );
 };
 
--- client/src/components/Pages/ServerError/index.js
+++ client/src/components/Pages/ServerError/index.js
@@ -2,17 +2,16 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 import error500 from '../../../assets/error500.png';
-
 import './style.css';
 
 const ServerError = () => {
   return (
-    <div>
+    <section>
       <img src={error500} alt="Server Error 500" className="serverError__img" />
       <Link to="/" className="error500__goHome">
         Return to Home Page
       </Link>
-    </div>
+    </section>
   );
 };
 
