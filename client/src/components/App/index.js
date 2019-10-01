c8b4e07d1dbf5a846c5237cc620ebbd3311ada41
FadiAlamassi <alamassifadi@gmail.com>
2019-10-01 23:00:52 +0300

    remove import test relates #6

--- client/src/components/App/index.js
+++ client/src/components/App/index.js
@@ -1,19 +1,14 @@
 import React from 'react';
 import { NotificationContainer } from 'react-notifications';
-import { BrowserRouter, Route } from 'react-router-dom';
-import Login from '../Pages/login/index';
 
 import 'react-notifications/lib/notifications.css';
 import './style.css';
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-        <NotificationContainer />
-        <Route path="/login" render={props => <Login {...props} />} />
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <NotificationContainer />
+    </div>
   );
 }
 
