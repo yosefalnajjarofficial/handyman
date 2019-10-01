95470a16b804fc5d3cdfb78054dac04af5f0fdff
hashemt <hashemta77@gmail.com>
2019-10-02 00:06:47 +0300

    handle page when the user dose not has jobs Relates #16

--- client/src/components/Pages/JobsPage/index.js
+++ client/src/components/Pages/JobsPage/index.js
@@ -1,22 +1,41 @@
 import React from 'react';
 import axios from 'axios';
+import { NotificationManager } from 'react-notifications';
 
 import JobCard from '../../common/JobCard';
+import './style.css';
 
 class JobPage extends React.Component {
   state = {
     jobsData: [],
+    exsitJob: true,
   };
 
   async componentDidMount() {
-    const response = await axios.get('/api/v1/jobs');
-    this.setState({
-      jobsData: response.data.data,
-    });
+    const { history } = this.props;
+    try {
+      const response = await axios.get('/api/v1/jobs');
+      if (response.data.data[0]) {
+        this.setState({
+          jobsData: response.data.data,
+        });
+      } else {
+        NotificationManager.info('There is No Jobs');
+        this.setState({ exsitJob: false });
+      }
+    } catch (err) {
+      history.push('/500');
+    }
   }
 
   render() {
-    const { jobsData } = this.state;
+    const { jobsData, exsitJob } = this.state;
+    if (!jobsData[0] && exsitJob) {
+      return <h3>...Loading</h3>;
+    }
+    if (!exsitJob) {
+      return <h1 className="noJobs">No Jobs</h1>;
+    }
     return jobsData.map(job => {
       const {
         id,
