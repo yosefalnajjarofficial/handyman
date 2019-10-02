b4ee0a32b194c6162750965a364ed69131200767
fares98 <faresalhello21@gmail.com>
2019-10-02 14:43:48 +0300

    handle the error and create the loading Relates #13

--- client/src/components/Pages/OneServices/index.js
+++ client/src/components/Pages/OneServices/index.js
@@ -12,63 +12,69 @@ class Services extends Component {
   state = {
     oneServicesData: [],
     name: 'building',
+    isExist: true,
   };
 
   async componentDidMount() {
+    const { history } = this.props;
     try {
       const services = await axios.get(`api/v1/service/1`);
       this.setState({ oneServicesData: services.data.data });
+      if (!services.data.data[0]) {
+        this.setState({ isExist: false });
+      }
     } catch (err) {
-      console.log(err);
+      history.push('/500');
     }
   }
 
-  clgData = () => {
-    const { oneServicesData } = this.state;
-    console.log(oneServicesData);
-  };
-
   render() {
-    const { oneServicesData, name } = this.state;
+    const { oneServicesData, name, isExist } = this.state;
     return (
       <section>
-        <SearchInput name="search" placeholder="Search" />
-
-        <div className="top-rated-cards">
-          {oneServicesData.map(element =>
-            element.name === name ? (
-              <Link
-                to={`profile/${element.handyman_id}`}
-                className="topRated-link"
-              >
-                <SliderCard
-                  key={element}
-                  handymanName={element.username}
-                  handymanBio={element.description}
-                  handymanService={element.name}
-                  rate={3}
-                  onSliderCardClick={this.clgData}
-                />
-              </Link>
-            ) : null
-          )}
-        </div>
-
-        <div className="handyman-cards">
-          {oneServicesData.map(element =>
-            element.name === name ? (
-              <Link to={`profile/${element.handyman_id}`} className="card-link">
-                <HandymanCard
-                  key={element}
-                  HandymanName={element.username}
-                  handymanBio={element.description}
-                  handymanService={element.name}
-                  rate={3}
-                />
-              </Link>
-            ) : null
-          )}
-        </div>
+        {!oneServicesData[0] && isExist && <h3>...Loading</h3>}
+        {!isExist && <h1>No handymans for this services at yet </h1>}
+        {oneServicesData[0] && (
+          <span>
+            <SearchInput name="search" placeholder="Search" />
+            <div className="top-rated-cards">
+              {oneServicesData.map(element =>
+                element.name === name ? (
+                  <Link
+                    to={`profile/${element.handyman_id}`}
+                    className="topRated-link"
+                  >
+                    <SliderCard
+                      key={element}
+                      handymanName={element.username}
+                      handymanBio={element.description}
+                      handymanService={element.name}
+                      rate={3}
+                    />
+                  </Link>
+                ) : null
+              )}
+            </div>
+            <div className="handyman-cards">
+              {oneServicesData.map(element =>
+                element.name === name ? (
+                  <Link
+                    to={`profile/${element.handyman_id}`}
+                    className="card-link"
+                  >
+                    <HandymanCard
+                      key={element}
+                      HandymanName={element.username}
+                      handymanBio={element.description}
+                      handymanService={element.name}
+                      rate={3}
+                    />
+                  </Link>
+                ) : null
+              )}
+            </div>
+          </span>
+        )}
       </section>
     );
   }
