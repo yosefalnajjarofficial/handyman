b5b927e19832cd97d81c67012f345e2b5a1fbecf
yosefalnajjarofficial <yosefalnajjarofficial@gmail.com>
2019-10-08 15:09:40 +0300

    added images for all the services

--- client/src/components/Pages/Services/index.js
+++ client/src/components/Pages/Services/index.js
@@ -5,6 +5,8 @@ import PropTypes from 'prop-types';
 import SearchInput from '../../common/SearchInput';
 import ServiceCard from '../../common/serviceCard';
 
+import servicePic from '../../utils/servicesPics';
+
 class Services extends Component {
   state = {
     servicesData: [],
@@ -55,7 +57,7 @@ class Services extends Component {
                 onServiceClick={this.handleClick}
                 serviceId={element.id}
                 serviceName={element.name}
-                serviceImage="https://static.wixstatic.com/media/496ffb_8b6064c94518461aaee56a4a17408300.jpg"
+                serviceImage={servicePic(element.name)}
                 key={element.name}
               />
             ) : null
--- server/controllers/routes/auth.js
+++ server/controllers/routes/auth.js
@@ -1,3 +1,4 @@
-module.exports = (req, res) => {
+ module.exports = (req, res) => {
   res.send(req.user);
 };
+
