642de61f69b3b66a99b4249f6e048be64d8623be
amoodaa <amoodaa@gmail.com>
2019-10-08 10:20:41 +0300

    comments yosef

--- server/db/config/build.sql
+++ server/db/config/build.sql
@@ -4,7 +4,7 @@ DROP TABLE IF EXISTS users, handyman, services, jobs;
 
 CREATE TABLE users(
   id SERIAL PRIMARY KEY NOT NULL,
-  username VARCHAR(255) NOT NULL,
+  username VARCHAR(255) NOT NULL, -- should be unique
   email VARCHAR(255) UNIQUE NOT NULL,
   phone INTEGER NOT NULL,
   password VARCHAR(255) NOT NULL,
